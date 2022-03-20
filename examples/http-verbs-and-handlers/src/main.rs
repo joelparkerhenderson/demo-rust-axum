064c985bb1753462c8d84ae0ba57f68b92900ba0
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-03-20 16:57:35 +0000

    Add examples: HTTP verbs and handlers

---
+++ examples/http-verbs-and-handlers/src/main.rs
@@ -0,0 +1,70 @@
+//! Demo of Rust and axum web framework.
+//!
+//! <https://github.com/joelparkerhenderson/demo-rust-axum>
+//!
+//! For more see the file `README.md` in the project root.
+
+/// Use axum capabities.
+use axum::routing::get;
+use axum::handler::Handler;
+
+#[tokio::main]
+async fn main() {
+    // Build our application by creating our router.
+    let app = axum::Router::new()
+        route("/foo",
+            get(get_foo)
+            .put(put_foo)
+            .patch(patch_foo)
+            .post(post_foo)
+            .delete(delete_foo)
+        );
+
+    // Run our application as a hyper server on http://localhost:3000.
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+////
+// Demo axum handlers with HTTP verbs and handlers.
+//
+//
+// The axum route has functions for HTTP verbs:
+// GET, PUT, PATCH, POST, DELETE.
+//
+// We use the naming convention of `get_foo`, `put_foo`, etc.
+//
+// These demo handlers in this section simply return a string.
+////
+
+/// axum handler for "GET /foo" which returns a string message.
+/// This shows our naming convention for HTTP GET handlers.
+pub async fn get_foo() -> String {
+    "GET foo".to_string()
+}
+
+/// axum handler for "PUT /foo" which returns a string message.
+/// This shows our naming convention for HTTP PUT handlers.
+pub async fn put_foo() -> String {
+    "PUT foo".to_string()
+}
+
+/// axum handler for "PATCH /foo" which returns a string message.
+/// This shows our naming convention for HTTP PATCH handlers.
+pub async fn patch_foo() -> String {
+    "PATCH foo".to_string()
+}
+
+/// axum handler for "POST /foo" which returns a string message.
+/// This shows our naming convention for HTTP POST handlers.
+pub async fn post_foo() -> String {
+    "POST foo".to_string()
+}
+
+/// axum handler for "DELETE /foo" which returns a string message.
+/// This shows our naming convention for HTTP DELETE handlers.
+pub async fn delete_foo() -> String {
+    "DELETE foo".to_string()
+}
