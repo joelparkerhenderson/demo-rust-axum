1381d02dbbea3ab0df2002e0f8698ec839a3a517
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-08-31 22:14:32 +0200

    Reformat

--- src/data.rs
+++ src/data.rs
@@ -24,10 +24,31 @@ use crate::book::Book;
 //         …
 // }).join().unwrap()
 // ```
-pub static DATA: Lazy<Mutex<HashMap<u32, Book>>> = Lazy::new(|| Mutex::new(
-    HashMap::from([
-        (1, Book { id: 1, title: "Antigone".into(), author: "Sophocles".into()}),
-        (2, Book { id: 2, title: "Beloved".into(), author: "Toni Morrison".into()}),
-        (3, Book { id: 3, title: "Candide".into(), author: "Voltaire".into()}),
-    ])
-));
+pub static DATA: Lazy<Mutex<HashMap<u32, Book>>> = Lazy::new(|| {
+    Mutex::new(HashMap::from([
+        (
+            1,
+            Book {
+                id: 1,
+                title: "Antigone".into(),
+                author: "Sophocles".into(),
+            },
+        ),
+        (
+            2,
+            Book {
+                id: 2,
+                title: "Beloved".into(),
+                author: "Toni Morrison".into(),
+            },
+        ),
+        (
+            3,
+            Book {
+                id: 3,
+                title: "Candide".into(),
+                author: "Voltaire".into(),
+            },
+        ),
+    ]))
+});
