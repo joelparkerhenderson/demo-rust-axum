40870dd0176e0f660463772d3f121aa8297dc8c6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-06-29 18:07:37 +0100

    Update example/epoch-handler-function

--- examples/epoch-handler-function/src/main.rs
+++ examples/epoch-handler-function/src/main.rs
@@ -1,10 +1,23 @@
+use axum::routing::get;
+
 #[tokio::main]
 async fn main() {
     // Build our application with a single route.
-    let app = axum::Router::new().route("/",
-        axum::routing::get(|| async { "Hello, World!" }));
+    let app = axum::Router::new()
+        .route("/epoch",
+            get(epoch)
+        );
 
     // Run our application as a hyper server on http://localhost:3000.
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+/// axum handler for "GET /epoch" which shows the current epoch time.
+/// This shows how to write a handler that uses time and can error.
+pub async fn epoch() -> Result<String, axum::http::StatusCode> {
+    match std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH) {
+        Ok(duration) => Ok(format!("{}", duration.as_secs())),
+        Err(_) => Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR)
+    }
 }
